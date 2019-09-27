d679ebe3084416826e099217a63217627ce605af
ks1c <ksic3784@gmail.com>
2019-09-27 10:51:39 -0400

    add gruvbox colors to dmenucmd

--- config.h
+++ config.h
@@ -12,19 +12,31 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
+
+/* DEFAULT COLORS */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+/* GRUVBOX COLORS */
+static const char col_gruvb8[]      = "#928374";
+static const char col_gruvb0[]      = "#282828";
+static const char col_gruvb11[]     = "#fabd2f";
+static const char col_gruvb15[]     = "#ebdbb2";
+static const char col_gruvb237[]    = "#3c3836";
+
+
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
+	/*               fg           bg         border   */
 	//[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	//[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 
 	// GRUVBOX
-	[SchemeNorm] = { "#928374", "#282828", "#282828" },
-	[SchemeSel]  = { "#ebdbb2", "#3c3836", "#928374" },
+	[SchemeNorm] = { col_gruvb8, col_gruvb0, col_gruvb0 },
+	[SchemeSel]  = { col_gruvb15, col_gruvb237, col_gruvb8 },
 };
 
 /* tagging */
@@ -67,7 +79,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gruvb0, "-nf", col_gruvb8, "-sb", col_gruvb237, "-sf", col_gruvb11, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 static const char *ssselectioncmd[]  = { "screenshot", "selection", NULL };
